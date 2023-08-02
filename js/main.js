7781d5c490682b08143e382e14f15ce6e080bd4c
Jorge Cano <canomelero1@correo.ugr.es>
2023-08-02 12:41:03 +0200

    problema de los puntos solucionado

--- js/main.js
+++ js/main.js
@@ -1,36 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     const pantalla = document.querySelector(".num-pantalla");
-    let cadena = "";    // numero que se va a ir imprimiendo en la pantalla de la calculadora
-    let numero = "";    // numero que se verá desde la consola del navegador
+    let cadena = "";      // numero que se va a ir imprimiendo en la pantalla de la calculadora
+    let numero = "";      // numero que se verá desde la consola del navegador
+    let cont_punto = 0;   // variale para controlar que el número tenga máximo "un punto"
     const arrayBotones = [];
 
     // Se añaden los operadores
-    arrayBotones.push(document.querySelector("#suma"));
-    arrayBotones.push(document.querySelector("#resta"));
-    arrayBotones.push(document.querySelector("#division"));
-    arrayBotones.push(document.querySelector("#multiplicacion"));
-    arrayBotones.push(document.querySelector("#modulo"));
+    for(let i = 1; i <= 5; i++) {
+        arrayBotones.push(document.querySelector(`#operacion${i}`));
+    }
+
     arrayBotones.push(document.querySelector("#punto"));
     arrayBotones.push(document.querySelector("#borrar"));
     arrayBotones.push(document.querySelector("#resolver"));
     
     // Se añaden los números
-    arrayBotones.push(document.querySelector("#num0"));
-    arrayBotones.push(document.querySelector("#num1"));
-    arrayBotones.push(document.querySelector("#num2"));
-    arrayBotones.push(document.querySelector("#num3"));
-    arrayBotones.push(document.querySelector("#num4"));
-    arrayBotones.push(document.querySelector("#num5"));
-    arrayBotones.push(document.querySelector("#num6"));
-    arrayBotones.push(document.querySelector("#num7"));
-    arrayBotones.push(document.querySelector("#num8"));
-    arrayBotones.push(document.querySelector("#num9"));
+    for(let i = 0; i <= 9; i++) {
+        arrayBotones.push(document.querySelector(`#num${i}`));
+    }
 
 
     // Se recorre el array hasta encontrar el botón sobre el que se ha pulsado
-    const tam_array = arrayBotones.length;
-    for(let i = 0; i < tam_array; i++) {
+    for(let i = 0; i < arrayBotones.length; i++) {
         arrayBotones[i].addEventListener('click', cargarValor);
     }
 
@@ -46,17 +38,28 @@ document.addEventListener("DOMContentLoaded", () => {
                 boton = "*";
             }
 
+            if(boton === ".") {
+                cont_punto++;
+            }
+
+            if(cont_punto > 1) {
+                agregarPantalla("SyntaxError");
+                return;     // Si se introducen dos puntos, la función lanza un error
+            }
+
             numero += boton;
             imprimirDatos(numero, cadena);
         }
         else if(boton === "=") {
             let resultado = eval(numero);
             cadena = resultado;
+            numero = resultado;
             imprimirDatos(resultado, cadena);
         }
         else if(boton === "C") {
             numero = "";
             cadena = "";
+            cont_punto = 0;
             imprimirDatos(numero, cadena);
         } 
     }
